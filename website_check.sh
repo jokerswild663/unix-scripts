0131f223db2ab85e09188c9293e53a6ad5eb6b11
Jeff Wyckoff <jokerswild663@gmail.com>
2014-08-12 13:35:16 -0700

    adding timeout capabilities and using fping to test reachability

--- website_check.sh
+++ website_check.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 Email=""
 Website=""
+Timeout=200
 
 
-while getopts :s:e: opt; do
+while getopts :s:e:t: opt; do
 case $opt in
 	s) Website="${OPTARG}" ;;
     e) Email="${OPTARG}" ;;
+    t) Timeout="${OPTARG}" ;;
 esac
 done
+
+Result=`fping -t $Timeout $Website | awk {'print $3'}`
+
